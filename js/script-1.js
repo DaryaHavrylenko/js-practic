4e4305fe7976e6e6766f68ed2316f780112d7500
Darya <HavrylenkoIT@gmail.com>
2022-07-20 00:53:38 +0300

    HW

--- js/script-1.js
+++ js/script-1.js
@@ -52,5 +52,88 @@
 // 	For “User” show “Good evening, dear User!”
 // 	For “Admin” show “Good evening, dear Admin!
 
-const hours = new Date().getHours();
+
+let userLogin = prompt("plese enter your login");
+
+console.log(userLogin);
+
+if (userLogin === "" || userLogin === null) {
+    alert('Canceled');
+} else if (userLogin.length < 4) {
+    let message = `I don't know any users having name length less than 4 symbols`;
+    alert(message);
+} else if (userLogin.toLowerCase() === "User".toLowerCase() || userLogin.toLowerCase() === "Admin".toLowerCase()) {
+    
+    let userPassword = prompt("Please enter your pasword");
+    if (userPassword === "" || !userPassword) {
+        alert('Canceled');
+    } else if (userLogin.toLowerCase() === "User".toLowerCase() && userPassword === "UserPass") {
+        console.log('user come in');
+    } else if (userLogin.toLowerCase() === "Admin".toLowerCase() && userPassword === "RootPass") {
+        console.log('admin come in');
+    } else {
+        alert('Wrong password');
+    }
+       }
+else {
+    let message = `I don’t know you`;
+    alert(message);
+}
+
+
+if (userLogin.toLowerCase() === "User".toLowerCase()) {
+            const hours = new Date().getHours();
 console.log(hours);
+    if (hours > 5 && hours < 20) {
+
+    alert('Good day, dear User!');
+} else {
+   alert('Good evening, dear User!');
+    }
+
+}
+
+if (userLogin.toLowerCase() === "Admin".toLowerCase()) {
+            const hours = new Date().getHours();
+console.log(hours);
+    if (hours > 5 && hours < 20) {
+        alert('Good day, dear Admin!');
+    }
+    else {
+        alert('Good evening, dear Admin!');
+    }
+}
+
+// var Valeri
+// const userLogin = prompt('Please enter login');
+// const hours = new Date().getHours();
+// if (userLogin === '' || userLogin === null) {
+//     alert('Canceled');
+// }
+// else if (userLogin.length < 4) {
+//     alert("I don't know any users having name length less than 4 symbols");
+// }
+// else if (userLogin === 'User' || userLogin === 'Admin') {
+//     const password = prompt('Please enter password')
+//     if (password === '' || password === null) {
+//         alert('Canceled');
+//     }
+//     else if (userLogin === 'User' && password === 'UserPass') {
+//         if (hours > 5 && hours < 20) {
+//             alert(`Good day, dear ${userLogin}!`);
+//         }
+//         else {
+//             alert(`Good evening, dear ${userLogin}!`);
+//         }
+//     }
+//     else if (userLogin === 'Admin' && password === 'RootPass') {
+//         if (hours > 5 && hours < 20) {
+//             alert(`Good day, dear ${userLogin}!`);
+//         }
+//         else {
+//             alert(`Good evening, dear ${userLogin}!`);
+//         }
+//     }
+//     else { alert('Wrong password'); }
+// }
+// else { alert("I don't know you"); }
