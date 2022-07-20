d12430ae45156b67c2bd4931ba42e58047bce594
Darya <HavrylenkoIT@gmail.com>
2022-07-20 14:19:17 +0300

    js-1

--- js/script-1.js
+++ js/script-1.js
@@ -1,4 +1,7 @@
 "use strict"
+// практика 16.07
+
+// задача1
 // You need to calculate amount of tip to give in a restaurant/cafe.
 // 	Workflow:
 // 1.	User inputs check sum. (Use “prompt” function).
@@ -31,6 +34,77 @@
 //         alert(message);
 //     }
 // }
+
+// задача2
+// Find middle character of the word.
+// User is going to input a word. Your task is to find the middle character of this word.
+// If the word’s length is odd - return the middle character.If word’s length is even - return the middle 2 characters.
+// You should validate for empty value and for value with spaces only, in that case show message: “Invalid value”.
+// For user input use “prompt” function. For displaying result use “alert” function.
+
+// Examples:
+// 	For user input “test” should return “es”
+// 	For user input “character” should return “a”
+// 	For user input “B” should return “B”
+// 	For user input “” should return “Invalid value”
+// For user input “       “ should return “Invalid value”
+
+// const word = prompt("input a word please");
+
+// if (word.trim() === "" || word === null) {
+//     alert("Invalid value");
+// } else {
+//     if (word.length % 2 === 0) {
+//         const lengthWord = word.length / 2;
+//         console.log(word.slice(lengthWord - 1, lengthWord + 1));
+//     } else {
+//         const lengthWord = word.length / 2;
+//         console.log(word.slice(lengthWord, lengthWord + 1));
+//     }
+// }
+
+// задача3
+// const a = ["a", "b", "c"];
+// const b = [1, [2, 3]];
+// const c = 0;
+// Result: ["a", "b", "c", 0, 1, 2, 3];
+
+// var1
+// const d = b.flat();
+// console.log(d);
+
+// const e = a.concat(d);
+// console.log(e);
+
+// const f = e.splice(3, 0, c);
+// console.log(e);
+ 
+// var2
+// const d = [b[0]].concat(b[1]);
+// console.log(d);
+// console.log(a.concat(c, d));
+
+// task4
+// const a = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+// Result: ["a", "b", "c", "a", "b", "c", "g", "h"];
+
+// var1
+// const b = a.splice(3, 3);
+// console.log(a);
+// const d = a.splice(3, 0, "a", "b", "c");
+// console.log(a);
+
+
+// var2
+
+// const b = a.splice(3, 3, 'a', 'b', 'c');
+// console.log(a, b);
+
+// var3
+// console.log(a.copyWithin(3, 0, 3));
+
+
 
 // HW
 // Write the code which verify user rights.
